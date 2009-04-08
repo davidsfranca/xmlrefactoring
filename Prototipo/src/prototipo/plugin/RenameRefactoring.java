5cbafa5f7ed18c43f08a1ea55b2d0aff9dcd8c3d
guilhermesalerno <guilhermesalerno@d5abb842-12f0-11de-a3d3-adbb9f2ee4f5>
2009-04-08 20:48:22 +0000

    Mudança de arquitetura para extension point de renomeações e correção do bug nativo do editor de não renomear elementos anônimos.

--- Prototipo/src/prototipo/plugin/RenameRefactoring.java
+++ Prototipo/src/prototipo/plugin/RenameRefactoring.java
@@ -29,17 +29,20 @@ import org.eclipse.wst.common.core.search.scope.WorkspaceSearchScope;
 import org.eclipse.wst.common.core.search.util.CollectingSearchRequestor;
 import org.eclipse.wst.xml.core.internal.provisional.document.IDOMElement;
 import org.eclipse.wst.xml.core.internal.search.XMLComponentDeclarationPattern;
+import org.eclipse.wst.xml.core.internal.search.XMLComponentReferencePattern;
 import org.eclipse.wst.xsd.ui.internal.refactor.RefactoringComponent;
 import org.eclipse.wst.xsd.ui.internal.refactor.RefactoringMessages;
 import org.eclipse.wst.xsd.ui.internal.refactor.TextChangeManager;
+import org.eclipse.wst.xsd.ui.internal.refactor.rename.SortingSearchRequestor;
 import org.eclipse.wst.xsd.ui.internal.refactor.util.TextChangeCompatibility;
+import org.eclipse.xsd.XSDNamedComponent;
 
 public class RenameRefactoring extends Refactoring{
 
 	private RefactoringComponent selectedComponent;
 	private TextChangeManager manager = new TextChangeManager();
 	private String newName;
-	
+
 	public String getNewName() {
 		return newName;
 	}
@@ -51,90 +54,33 @@ public class RenameRefactoring extends Refactoring{
 	public RenameRefactoring(RefactoringComponent selectedComponent){
 		this.selectedComponent = selectedComponent;
 	}
-	
+
 	@Override
-	/**
-	 * MŽtodo que cuida de toda a l—gica da refatora‹o
-	 * Praticamente copiado de org.eclipse.wst.xsd.ui.internal.refactor.rename.RenameComponentProcessor
-	 */
 	public RefactoringStatus checkFinalConditions(IProgressMonitor pm)
-			throws CoreException, OperationCanceledException {
-		
-		String fileStr = selectedComponent.getElement().getModel().getBaseLocation();
-		IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(fileStr));
-		
-		String componentName = selectedComponent.getName();
-		String componentNamespace = selectedComponent.getNamespaceURI();
-		QualifiedName elementQName = new QualifiedName(componentNamespace, componentName);
-		QualifiedName typeQName = selectedComponent.getTypeQName();
-		
-		SearchScope scope = new WorkspaceSearchScope();
-		if (file != null) {
-			scope = new SelectionSearchScope(new IResource[]{file});
-		}
-		CollectingSearchRequestor requestor = new CollectingSearchRequestor();
-		SearchPattern pattern = new XMLComponentDeclarationPattern(file, elementQName, typeQName);
-		SearchEngine searchEngine = new SearchEngine();
-        HashMap map = new HashMap();
-		searchEngine.search(pattern, requestor, scope, map, new NullProgressMonitor());
-		List results = requestor.getResults();
-		
-		// more than one declaration found, so use offset as additional check
-		Position offsetPosition = null;
-		if (results.size() > 1) {
-		    IDOMElement selectedElement = selectedComponent.getElement();
-		    if (selectedElement != null) {
-		    	int startOffset = selectedElement.getStartOffset();
-		    	offsetPosition = new Position(startOffset, (selectedElement.getEndOffset() - startOffset));
-		    }
-		}
-		
-		String quotedNewName = quoteString(newName);
-		
-		for (Iterator iter = results.iterator(); iter.hasNext();) {
-			SearchMatch match = (SearchMatch) iter.next();
-			if (match != null) {
-				boolean addTextChange = true;
-				// additional check to verify correct declaration is changed
-				if (offsetPosition != null) {
-					addTextChange = offsetPosition.overlapsWith(match.getOffset(), match.getLength());  
-				}
-				if (addTextChange) {
-					TextChange textChange = manager.get(match.getFile());
-					
-					ReplaceEdit replaceEdit = new ReplaceEdit(match.getOffset(), match.getLength(), quotedNewName);
-					String editName = RefactoringMessages.getString("RenameComponentProcessor.Component_Refactoring_update_declatation");
-					TextChangeCompatibility.addTextEdit(textChange, editName, replaceEdit);
-				}
-				createXSLTData(match);
-			}
-		}
+	throws CoreException, OperationCanceledException {
 		return new RefactoringStatus();
 	}
-	
-	private void createXSLTData(SearchMatch match){
-	
-	}
+
 
 	@Override
 	public RefactoringStatus checkInitialConditions(IProgressMonitor pm)
-			throws CoreException, OperationCanceledException {
+	throws CoreException, OperationCanceledException {
 		// TODO Auto-generated method stub
 		return new RefactoringStatus();
 	}
 
 	@Override
 	public Change createChange(IProgressMonitor pm) throws CoreException,
-			OperationCanceledException {
+	OperationCanceledException {
 		try{
-		pm.beginTask("Creating change...", 1);
-		CompositeChange compositeChange = new CompositeChange("exemplo", manager.getAllChanges());
-		return compositeChange;
+			pm.beginTask("Creating change...", 1);
+			CompositeChange compositeChange = new CompositeChange("exemplo", manager.getAllChanges());
+			return compositeChange;
 		} finally{
 			pm.done();
 		}
 	}
-	
+
 	public static String quoteString(String value) {
 		value = value == null ? "" : value;
 
@@ -148,7 +94,7 @@ public class RenameRefactoring extends Refactoring{
 		}
 		return sb.toString();
 	}
-	
+
 	@Override
 	public String getName() {
 		return "XSDRenameRefactoring";
